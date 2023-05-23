d93a99d492b7051f4fcde895001ff81eb5c0b745
Wenxuan Feng <279357596@qq.com>
2023-05-23 22:05:14 +0800

    feat: add v2 build images

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 func main() {
@@ -40,11 +42,49 @@ func handleWebhook(c *gin.Context)  {
 	c.JSON(http.StatusOK, gin.H{"message": "Docker image build and execution complete"})
 }
 
+func buildDockerImageV2(ref, commitID, imageName string) {
+	ctx := context.Background()
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		log.Fatalf("Failed to create Docker client: %s", err.Error())
+	}
+
+	buildOptions := types.ImageBuildOptions{
+		Tags:        []string{imageName},
+		BuildArgs:   map[string]*string{"REF": &ref, "COMMIT_ID": &commitID},
+		ContextPath: ".",
+	}
+
+	buildResponse, err := cli.ImageBuild(ctx, nil, buildOptions)
+	if err != nil {
+		log.Fatalf("Failed to build Docker image: %s", err.Error())
+	}
+
+	defer buildResponse.Body.Close()
+
+	// 解析构建日志并进行适当处理
+	// ...
+
+	log.Printf("Docker image built successfully: %s", imageName)
+}
+
 func buildDockerImage(ref, commitID, imageName string)  {
 	// TODO: 实现Docker镜像构建逻辑，可以使用Docker命令行工具或Docker SDK for Go
 	log.Printf("Building Docker image for ref %s, commit ID %s, image name %s", ref, commitID, imageName)
 	// 构建镜像的具体命令
-	
+
+		// 构建镜像的具体命令
+		cmd := exec.Command("docker", "build", "-t", imageName, "--build-arg", "REF="+ref, "--build-arg", "COMMIT_ID="+commitID, ".")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		
+		if err != nil {
+			log.Printf("Docker image build failed: %s", err)
+		}
+
+		log.Printf("Docker image build complete")	
 }
 
 func runDockerImage(imageName string) {
